10cb74208191b0d9b9bee114005a7232e4b6e9ab
Blake Robertson <104099448+firewry@users.noreply.github.com>
2024-02-08 10:36:40 -0500

    Add files via upload

---
+++ html-insert/inserts/footer-inserts.js
@@ -0,0 +1,7 @@
+let Footer = `
+    <div class="foot">
+        <p class="footer-copy">Copyright &copy; firewry 2022 - 2023</p>
+        <a class="bg-img-credit-link" href="https://www.toptal.com/designers/subtlepatterns/" target="_blank">BG IMAGES</a>
+    </div>
+`;
+document.getElementById("footer").innerHTML = Footer;
---
+++ html-insert/inserts/head-inserts.js
@@ -0,0 +1,5 @@
+let Head = `
+    <title>firewry</title>
+    <link rel="icon" type="image/x-icon" href="content/favicons/FLAME-Favicon.png">
+`;
+document.getElementById("head").innerHTML += Head;
