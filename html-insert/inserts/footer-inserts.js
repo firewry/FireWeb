5b4a6cbe9302dee42d3099c6c90a5f5040189d6e
Blake Robertson <104099448+firewry@users.noreply.github.com>
2024-02-09 10:38:44 -0500

    Add files via upload

--- html-insert/inserts/footer-inserts.js
+++ html-insert/inserts/footer-inserts.js
@@ -1,6 +1,6 @@
 let Footer = `
     <div class="foot">
-        <p class="footer-copy">Copyright &copy; firewry 2022 - 2023</p>
+        <p class="footer-copy">Copyright &copy; firewry 2022 - 2024</p>
         <a class="bg-img-credit-link" href="https://www.toptal.com/designers/subtlepatterns/" target="_blank">BG IMAGES</a>
     </div>
 `;
